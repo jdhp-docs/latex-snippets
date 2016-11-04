924b5d6dd7c0b81824be5fab51334f90b3b284ef
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-11-04 15:43:41 +0100

    Fix a bug.

--- init-skeleton.sh
+++ init-skeleton.sh
@@ -33,6 +33,8 @@ TODO_DOCUMENT_WEB_SITE_URL="http://www.jdhp.org/documentation_en.html#${TODO_DOC
 TODO_DOCUMENT_HTML_URL="http://www.jdhp.org/docs/${TODO_DOCUMENT_FILE_BASE_NAME}/${TODO_DOCUMENT_FILE_BASE_NAME}.html"
 TODO_DOCUMENT_PDF_URL="http://www.jdhp.org/dl/pdf/${TODO_DOCUMENT_FILE_BASE_NAME}.pdf"
 
+TODO_HTML_TMP_DIR="html_tmp"
+
 
 # SETUP GIT ###################################################################
 
